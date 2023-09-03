3b8d5f22487bc5a50b0a19a5254a2b0c1be79528
mk7mrunu <mrunmaykumbhar@gmail.com>
2023-09-03 21:08:25 +0530

    Updating News App

---
+++ src/components/NewsItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const NewsItem = (props) => {
+    let{description, imageUrl, newsUrl, author, date, source} = props;
+    return (
+      <div className='my-3'>
+        <div className="card">
+          <div style={{display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0'}}>
+          <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+             <img src={!imageUrl?"https://cdn.ndtv.com/common/images/ogndtv.png":imageUrl} class="card-img-top" alt="..."/>
+            <div className="card-body">
+             <p className="card-text">{description}</p>
+             <p className="card-text"><small className="text-body-secondary">By {!author?"Unknown": author} on {new Date(date).toGMTString()}</small></p>
+             <a rel="noreferrer" href={newsUrl} target="_blank" class="btn btn-sm btn-dark">Read More</a>
+            </div>
+        </div>
+      </div>
+    )
+  }
+
+
+export default NewsItem
